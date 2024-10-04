c3c97850fa4c69896f6332ac9a1c181f68311d0b
4-gent <spicyrice09@gmail.com>
2024-10-04 00:52:38 -0700

    most recent iteration

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -14,23 +14,23 @@ import '../styles/navbar.css'
 export default function Navigation() {
     return(
         // Wrapping the Navbar component in a div
-        <div>
+        <div className=''>
             {/* Navbar component with properties for collapse, expand, background color, and variant */}
-            <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+            <Navbar collapseOnSelect expand='lg' className="navbar">
                 {/* Container component to make the Navbar fluid */}
                 <Container fluid>
                     {/* Navbar.Brand component for the home link */}
-                    <Navbar.Brand href='/'>Home</Navbar.Brand>
+                    <Navbar.Brand className='nav-text' href='/'>Home</Navbar.Brand>
                     {/* Navbar.Toggle component for the collapsible button */}
-                    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                    <Navbar.Toggle aria-controls='responsive-navbar-nav' className='nav-toggle'  />
                     {/* Navbar.Collapse component to handle responsive behavior */}
                     <Navbar.Collapse id='responsive-navbar-nav'>
                         {/* Nav component to align links to the right */}
                         <Nav className='ms-auto'>
                             {/* Nav.Link component for the login link */}
-                            <Nav.Link href='/login'>Login</Nav.Link>
+                            <Nav.Link className='nav-text' href='/login'>Login</Nav.Link>
                             {/* Nav.Link component for the register link */}
-                            <Nav.Link href='/register'>Register</Nav.Link>
+                            <Nav.Link className='nav-text' href='/register'>Register</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
